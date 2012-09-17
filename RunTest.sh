beb5f5b12647aca61f0aebf21d8947df939675a5
George Lewis <ghl227@nyu.edu>
2012-09-17 10:41:51 -0400

    Adding HistFactory Unit Test

---
+++ RunTest.sh
@@ -0,0 +1,61 @@
+
+
+#ROOTVERSIONS="root-histfactory-dev root-roostats-dev root-roostats-branch root-5.32.00-patches root-trunk"
+ROOTVERSIONS="root-roostats-git root-roostats-branch root-trunk"
+
+TESTS="example example_Expression example_params example_Ultimate example_ShapeSys example_ShapeSys2D"
+PYTHONSCRIPTS="example.py"
+CPPSCRIPTS="example.C"
+
+
+
+
+# Create the logs
+for build in $ROOTVERSIONS
+do
+    for version in $TESTS
+    do
+	source /usr/local/root_versions/${build}/bin/thisroot.sh
+	/usr/local/root_versions/${build}/bin/hist2workspace config/${version}.xml 2>&1 | tee logs/${build}_${version}.log 
+	# Now, get the reduced version of the log
+	sed -n '/**MIGRAD/,/**MINOS:/p' logs/${build}_${version}.log >logs/${build}_${version}_reduced.log
+    done
+done
+
+# Create a summary file for the tests
+SUMMARY=tests/Summary.txt
+echo "Summary:" >${SUMMARY}
+
+# Now, do the tests
+for buildA in $ROOTVERSIONS
+do
+    for buildB in $ROOTVERSIONS
+    do
+
+	if [ $buildA = $buildB ]
+	then
+	    continue
+	fi
+
+	for version in $TESTS
+	do
+	    logA=logs/${buildA}_${version}_reduced.log
+	    logB=logs/${buildB}_${version}_reduced.log
+	    DIFF=$(diff -b -B $logA $logB)
+	    if [ "$DIFF" = "" ]
+	    then
+		echo "Passed Test for ${version}: $buildA $buildB"
+		echo "Passed Test for ${version}: $buildA $buildB" >>${SUMMARY}
+	    else
+		echo "Failed Test for ${version}: $buildA $buildB"
+		echo "Failed Test for ${version}: $buildA $buildB" >>${SUMMARY}
+		echo "${DIFF}" >tests/diff_${buildA}_${buildB}_${version}.log
+	    fi
+	done
+    done
+done
+
+
+
+
+
